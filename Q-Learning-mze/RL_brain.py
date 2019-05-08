c7c2ebdcbd0e0c8eff2579b1f29ee611c2a79dc2
BOSON <suda1527403057@163.com>
2019-05-08 14:33:59 +0800

    Create RL_brain.py

---
+++ Q-Learning-mze/RL_brain.py
@@ -0,0 +1,55 @@
+"""
+这个文件主要是Q函数的学习，用的是表格的方式进行存储的
+表格的每一行代表一个状态，每一列代表一个动作
+"""
+
+import numpy as np
+import pandas as pd
+import math
+
+
+class QLearningTable:
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = actions 
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+        self.cost_his = []
+
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+        if np.random.uniform() < self.epsilon:
+            state_action = self.q_table.loc[observation, :]
+            action = np.random.choice(state_action[state_action == np.max(state_action)].index)
+        else:
+            action = np.random.choice(self.actions)
+        return action
+
+    def learn(self, s, a, r, s_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+        if s_ != 'terminal':
+            q_target = r + self.gamma * self.q_table.loc[s_, :].max()  
+        else:
+            q_target = r  
+        self.q_table.loc[s, a] += self.lr * (q_target - q_predict)
+        self.cost = math.fabs(q_target - q_predict)
+        self.cost_his.append(self.cost)
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0]*len(self.actions),
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            )
+            
+    def plot_cost(self):
+        import matplotlib.pyplot as plt
+        plt.plot(np.arange(len(self.cost_his)), self.cost_his)
+        plt.ylabel('Cost')
+        plt.xlabel('training steps')
+        plt.show()
